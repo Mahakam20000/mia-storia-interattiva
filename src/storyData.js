e0f9f603c86b910ca147745a1f2f6acbd56a8373
root <root@GNAOPC.localdomain>
2024-09-23 19:19:52 +0200

    tolto lo slash da storyData prima di images per provare ad usare percorsi relativi

--- src/storyData.js
+++ src/storyData.js
@@ -4,46 +4,46 @@ export const storyData = [
   {
     id: 1,
     text: "Sei a casa ed hai fame.",
-    image: "/images/1.jpg",
+    image: "images/1.jpg",
     options: [
-      { text: "Apri il frigorifero", nextId: 2, image: "/images/2.jpg" },
-      { text: "Apri la dispensa", nextId: 3, image: "/images/3.jpg" },
+      { text: "Apri il frigorifero", nextId: 2, image: "images/2.jpg" },
+      { text: "Apri la dispensa", nextId: 3, image: "images/3.jpg" },
     ],
   },
   {
     id: 2,
     text: "Nel frigo ci sono dei wurstel.",
-    image: "/images/2.jpg",
+    image: "images/2.jpg",
     options: [
-      { text: "Mangia i wurstel", nextId: 4, image: "/images/4.jpg" },
-      { text: "Non mangiare i wurstel", nextId: 5, image: "/images/5.jpg" },
+      { text: "Mangia i wurstel", nextId: 4, image: "images/4.jpg" },
+      { text: "Non mangiare i wurstel", nextId: 5, image: "images/5.jpg" },
     ],
   },
   {
     id: 3,
     text: "In dispensa trovi del pane in cassetta.",
-    image: "/images/3.jpg",
+    image: "images/3.jpg",
     options: [
-      { text: "Mangia il pane in cassetta", nextId: 6, image: "/images/6.jpg" },
-      { text: "Non mangiare il pane in cassetta", nextId: 5, image: "/images/5.jpg" },
+      { text: "Mangia il pane in cassetta", nextId: 6, image: "images/6.jpg" },
+      { text: "Non mangiare il pane in cassetta", nextId: 5, image: "images/5.jpg" },
     ],
   },
   {
     id: 4,
     text: "Cucini i wurstel e li mangi. Sono molto buoni. Vai a letto sazio.",
-    image: "/images/4.jpg",
+    image: "images/4.jpg",
     options: [],
   },
   {
     id: 5,
     text: "Non mangi nulla e vai a letto affamato.",
-    image: "/images/5.jpg",
+    image: "images/5.jpg",
     options: [],
   },
   {
     id: 6,
     text: "Mangi il pane in cassetta. Non è il massimo ma almeno non hai più fame. Vai a letto.",
-    image: "/images/6.jpg",
+    image: "images/6.jpg",
     options: [],
   },
 ];
